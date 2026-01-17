f96ced93d4283721925c8f9daa7fb0b3c8364bae
KevinIansyah <keviniansyah04@gmail.com>
2026-01-17 18:20:07 +0700

    fix: factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,12 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::create([
             'name' => 'Kevin Iansyah',
             'email' => 'keviniansyah04@gmail.com',
-            'password' => 'yayanpoe123',
+            'password' => Hash::make('yayanpoe123'),
             'role' => 'admin',
         ]);
     }
